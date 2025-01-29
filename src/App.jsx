0110ebc627813530bd73b5b0f0b80ff9b27a5a82
33SLueck <sven.lueck@dci-student.org>
2025-01-29 10:51:30 +0100

    json removed

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
   return (
     <div className="App">  
     <BrowserRouter>
-      <TopNav/>
-     <Data/>
-    
+      <TopNav/>  
       <Home/>
       <Services/>
       <About/>
