55e57d39c0d9e6acda93c7038cebe99e917c42cc
33SLueck <sven.lueck@dci-student.org>
2025-02-03 09:01:23 +0100

    Projects from json added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,9 @@ export default function Card(props) {
       <h2>{props.cardTitle}</h2>
       <img className="card_img" src={props.imgSrc} alt={props.imgAlt} />
       <div className="card_bottom">
-        <a href="#">{props.cardCode}</a>
+        <a href={props.cardCodeUrl}>{props.cardCode}</a>
         <div className="seperator"></div>
-        <a href="#">{props.cardTarget}</a>
+        <a href={props.cardTargetUrl}>{props.cardTarget}</a>
       </div>
     </div>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Card from "./Card";
 import projectLogo from "../assets/portfolio.png";
 
 export default function Cards() {
+  const basePath = process.env.REACT_APP_BASE_URL || "";
+   const [projects, setProjects] = useState([]);
+   useEffect(() => {
+      // Lade die JSON-Datei
+      fetch(`${basePath}/projects.json`)
+        .then((response) => response.json())
+        .then((data) => setProjects(data))
+        .catch((error) => console.error("Fehler beim Laden der Daten:", error));
+    }, []);
+  
   return (
     <div className="card_wrapper">
-      <Card
-        cardTitle={"Portfolio"}
-        cardCode={"Code"}
-        cardTarget={"Visit"}
-        imgSrc={projectLogo}
-        imgAlt={"ein Bild"}
-      />
+       {projects.map((project) => (
+          <Card key={project.id}
+          cardTitle={project.title}
+          cardCodeUrl={project.codeUrl}
+          cardTargetUrl={project.targetUrl}
+          cardCode={"Code"}
+          cardTarget={"Visit"}
+          imgSrc={`${basePath + project.imgSrc}`}
+          imgAlt={project.imgAlt}
+        />
+      ))}
+     
       <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
       <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
-      <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
-      <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
-      <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
-      <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
-      <Card cardTitle={"Test"} cardContent={"Das ist ein Test"} />
+     
     </div>
   );
 }
